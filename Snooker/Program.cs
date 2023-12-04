dc7e4c42272e5cf5821bccf05a1465d339e82822
tothtamas9 <71500475+tothtamas9@users.noreply.github.com>
2023-12-04 19:38:09 +0100

    Add project files.

---
+++ Snooker/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Snooker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Versenyzo> versenyzok = new List<Versenyzo>();
+            string[] olvasottVersenyzok = File.ReadAllLines("Source\\snooker.txt");
+            for (int i = 1; i < olvasottVersenyzok.Length; i++)
+            {
+                string[] tagok = olvasottVersenyzok[i].Split(';');
+                Versenyzo ujVersenyzo = new Versenyzo(int.Parse(tagok[0]), tagok[1], tagok[2], int.Parse(tagok[3]));
+                versenyzok.Add(ujVersenyzo);
+            }
+
+            //3.feladat: 
+            Console.WriteLine($"3. feladat: A világranglistán {versenyzok.Count()} versenyző szerepel");
+
+            //4.feladat:
+            double osszBevetel = 0;
+            int db = 0;
+
+            foreach (var i in versenyzok)
+            {
+                osszBevetel += i.Nyeremeny;
+                db++;
+            }
+            Console.WriteLine($"4. feladat: A versenyzők átlagosan {Math.Round(osszBevetel / db, 2)} fontot kerestek");
+
+            //5.feladat:
+
+            //6.feladat:
+            bool norveg = false;
+            Console.Write("6. feladat: ");
+            foreach (var i in versenyzok)
+            {
+                if (i.Orszag == "Norvégia")
+                {
+                    norveg = true;
+                    break;
+                }
+            }
+            if (norveg == true)
+            {
+                Console.WriteLine("A versenyzők között van norvég versenyző");
+            }
+            else
+            {
+                Console.WriteLine("A versenyzők között nincs norvég versenyző");
+            }
+
+            //7.feladat:
+        }
+    }
+}
---
+++ Snooker/Versenyzo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snooker
+{
+    public class Versenyzo
+    {
+        int helyezes;
+        string nev;
+        string orszag;
+        int nyeremeny;
+
+        public Versenyzo(int helyezes, string nev, string orszag, int nyeremeny)
+        {
+            this.helyezes = helyezes;
+            this.nev = nev;
+            this.orszag = orszag;
+            this.nyeremeny = nyeremeny;
+        }
+
+        public int Helyezes { get => helyezes; }
+        public string Nev { get => nev; }
+        public string Orszag { get => orszag; }
+        public int Nyeremeny { get => nyeremeny; }
+    }
+}
